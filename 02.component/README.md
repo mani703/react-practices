bc4328226e84a74283ed875b1f8e625c15088146
unknown <mani703@naver.com>
2021-07-27 12:56:43 +0900

    component #3

--- 02.component/README.md
+++ 02.component/README.md
@@ -1,12 +1,12 @@
 ## React Practices - Component
 
-### ex01: Property: Component 작성 
-### ex02: Component Styling         
+### ex01: Property: Component 작성 (Data Flow: Top->down) - Kanban Board #1
+### ex02: Component Styling                               - Kanban Board #2
 ### ex03: React Event               
 ### ex04: State I: Controlled vs Uncontrolled Component
-### ex05: State II: Stateful and Pure Component
+### ex05: State II: Stateful(주로 class component) and Pure Component(주로 funciton component)
 ### ex06: State III: Data Flow (Data Flow: Down->Top)
 ### ex07: Component Lifecycle + AJAX(fetch, axios)
 
-### Kanban Board
-### Guestbook SPA
+### Guestbook SPA [과제]
+### Gallery SPA [과제]
--- 02.component/ex01/src/02/FoodList.js
+++ 02.component/ex01/src/02/FoodList.js
@@ -3,14 +3,13 @@ import ListItem from './ListItem';
 
 export default class FoodList extends Component {
     render() {
-        const foods = [];
-        this.props.foods.forEach(function(food, index){
-           foods[index] = <ListItem key={food.no} name={food.name} quantity={food.quantity} />;
-        });
-
+        // const foods = [];
+        // this.props.foods.forEach(function(food, index){
+        //    foods[index] = <ListItem key={food.no} name={food.name} quantity={food.quantity} />;
+        // });
         return (
             <ul>
-                { foods } 
+                { this.props.foods.map((food) => <ListItem key={food.no} name={food.name} quantity={food.quantity} />) } 
             </ul>  
         );
     }    
