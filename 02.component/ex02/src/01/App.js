36d5a3ef5b41029144632ce238eb16743a4582c9
unknown <mani703@naver.com>
2021-07-29 12:04:14 +0900

    css styling

---
+++ 02.component/ex02/src/01/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function App() {
+    const h1Styles = {
+        height: 50,
+        padding: 5,
+        color: '#fff',
+        backgroudColor: '#ee9900'
+    };
+
+    return (
+        <div id='App'>
+            <h1 style={ h1Styles }>Inline Styling</h1>
+        </div>
+    );
+
+}
---
+++ 02.component/ex02/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = (env) => {
+    return {
+        mode: "none",
+        entry: path.resolve(`src/${env.src}/index.js`),
+        output: {
+            path: path.resolve('public'),
+            filename: 'bundle.js',
+            assetModuleFilename: 'assets/images/[hash][ext]'
+        },
+        module: {
+            rules: [{
+                test: /\.js$/i,
+                exclude: /node-modules/,
+                loader: 'babel-loader'
+            }, {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                    'style-loader', 
+                    { loader: 'css-loader', options: { modules: env['css-modules'] !== 'false' } }, 
+                    'sass-loader'
+                ]
+            }, {
+                test: /\.(png|gif|ipe?g|svg|ico|tiff?|bmp)$/i,
+                type: 'asset/resource'
+            }]
+        },
+        devtool: "eval-source-map",
+        devServer: {
+            contentBase: path.resolve('public'),
+            watchContentBase: true,
+            host: "0.0.0.0",
+            port: 9999,
+            inline: true,
+            liveReload: true,
+            hot: false,
+            compress: true,
+            historyApiFallback: true
+        }
+    };
+}
