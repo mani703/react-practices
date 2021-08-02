59bad9a2de798430c517fbe732ae170e4b3bdba7
unknown <mani703@naver.com>
2021-08-02 12:48:15 +0900

    property validator #1

---
+++ 02.component/ex01/src/04/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyComponent from './MyComponent';
+
+export default function() {
+    return (
+        <div id='App'>
+            <MyComponent props01={ 10 }/>
+        </div>
+    );
+}
