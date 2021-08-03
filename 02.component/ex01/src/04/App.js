54028f6e013499bf0cb322e6fa0b9a8915ac3522
unknown <mani703@naver.com>
2021-08-03 09:09:01 +0900

    kanban board

--- 02.component/ex01/src/04/App.js
+++ 02.component/ex01/src/04/App.js
@@ -4,7 +4,13 @@ import MyComponent from './MyComponent';
 export default function() {
     return (
         <div id='App'>
-            <MyComponent props01={ 10 }/>
+            <MyComponent /* props01={ '문자열' } : Not Required, Default Value Set */ 
+            props02={ 1 }
+            props03={ true }
+            props04={ { no:1 } }
+            props05={ [1, 2, 3, 4] }
+            props06={ () => '함수' }
+            />
         </div>
     );
 }
--- 02.component/ex01/src/04/MyComponent.js
+++ 02.component/ex01/src/04/MyComponent.js
@@ -1,16 +1,47 @@
 import React, { Fragment } from 'react';
 import { PropTypes } from 'prop-types';
 
-export default function MyComponent({ props01 }) {
+export default function MyComponent({ props01, props02, props03, props04, props05, props06 }) {
     return (
         <Fragment>
             <h2>Propoerty Validation</h2>
             <span>props01: { props01 ? props01 : '---not set---'}</span>
+            <br/>
+
+            <span>props02: { props02 ? props02 : '---not set---'}</span>
+            <br/>
+
+            <span>props03: { props03 ? `${props03}` : '---not set---'}</span>
+            <br/>
+
+            <span>props04: { props04 ? props04.no : '---not set---'}</span>
+            <br/>
+
+            <span>props05: { props05 ? props05.map((e, i) => <b key={i}>{e}{ ' ' }</b>) : '---not set---'}</span>
+            <br/>
+
+            <span>props06: { props06 ? props06() : '---not set---'}</span>
+            <br/>
         </Fragment>
     );
 }
 
 MyComponent.propTypes = {
     // [Built-in propTypes Validator: primitives]
-    props01: PropTypes.string
+    props01: PropTypes.string,
+    props02: propTypes.number.isRequired,
+    props03: propTypes.bool.isRequired,
+    props04: propTypes.object.isRequired,
+    props05: propTypes.array.isRequired,
+    props06: propTypes.func.isRequired,
+}
+
+// Default Value
+MyComponent.defaultProps = {
+    props01: '기본값',
+    props02: 10,
+    props03: true,
+    props04: {},
+    props05: [],
+    props06: () => {}, // dummy function
 }
