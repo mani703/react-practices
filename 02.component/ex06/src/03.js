bfb22733d2ce0359b1d7101eeddbd2bbf1a31755
DESKTOP-IQJE3QV\myeongman <mani703@naver.com>
2021-08-11 00:34:42 +0900

    test

--- 02.component/ex06/src/03.js
+++ 02.component/ex06/src/03.js
@@ -6,5 +6,4 @@ let state = {
 let updateOrder = Object.assign({}, state.order, {receive: '부산시 해운대구 우동'});
 
 updateOrder.payment.method="Mobile";
-
 console.log(state.order, updateOrder, updateOrder === state.order);
